e9a839bb8b1c208e53bbf117f7b384b284d94d37
sevalnuroncu <sevalnuroncu@gmail.com>
2024-02-23 10:18:38 +0300

    classes finished

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -8,8 +8,18 @@ namespace Classes
 {
     class Customer
     {
+        //field
+        //private string FistName;
+        private string _firstName;
+
+        //property
+        //auto prpperty
         public int  Id { get; set; }
-        public string FirstName { get; set; }
+        public string FirstName
+        {
+            get { return "Mrs."+ _firstName; }
+            set { _firstName = value; }
+        }
         public string LastName { get; set; }
         public string City { get; set; }
     }
